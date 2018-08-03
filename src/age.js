296f4d3fc85b8399a79f66addc0308d2edafddec
Scott Fraser <scottafraser@yahoo.com>
2018-08-03 10:29:24 -0700

    create method for age in seconds

--- src/age.js
+++ src/age.js
@@ -1,15 +1,24 @@
 export class Person {
   constructor(age, life) {
+  this.birdthday = birthday;
   this.age = age;
-  this.line2 = life;
+  this.life = life;
   
 }
 
 // Years of age,times 365, times 24, times 60, times 60. Then, number of days between your last birthday, and today. 
 // Multiply by 24, 60, and 60. Then the number of hours between your time of birth and now.
 
-  findAgeinSecons(ageYears){
-  
+  getAgeInSeconds(birthdayInput){
+    const birthday = new Date('birthdayInput');
+    const timeNow = new Date();
+    const ageInSeconds = (timeNow - birthday) * 0.001;
+
+    return ageInSeconds;
+  }
+
+  findAgeinSeconds(ageYears){
+  ageYears * 365 * 24 * 60
    return line
 
   }
