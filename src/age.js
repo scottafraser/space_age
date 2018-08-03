dd824a0a7070316cb4f11b3d0a2476b07a039956
Scott Fraser <scottafraser@yahoo.com>
2018-08-03 14:37:32 -0700

    create method and test for finding ages on other planets

--- src/age.js
+++ src/age.js
@@ -26,5 +26,27 @@ export class Person {
     });
     return planetAges;
   }
+  findYearInSeconds(){
+    let yearInSeconds = Math.floor(this.age * 365 * 24 * 60 * 60);
+
+    return yearInSeconds;
+    }
+
+  getExactAge(){
+    const birthday = new Date(this.birthday);
+    const timeNow = new Date();
+    const exactAge = Math.floor(timeNow - birthday) * 0.001;
+
+    return exactAge;
+  }
+
+  getPlanetAges(){
+    const earthAge = this.age;
+    const planetYears = [0.24, 0.62, 1.88, 11.86];
+    let planetAges = planetYears.map(function(yearValue) {
+      return (earthAge / yearValue).toFixed(2);
+    });
+    return planetAges;
+  }
 
 }
